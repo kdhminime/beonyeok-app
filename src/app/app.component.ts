10bb095c88e168cfedcba752954c0a84060ce7ee
Dohyun Kim <56704790+kdhminime@users.noreply.github.com>
2024-02-07 20:26:21 -0700

    sign up dialog component created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { AmplifyAuthenticatorModule } from '@aws-amplify/ui-angular';
+
+// Import components
+import { SignUpDialogComponentComponent } from './components/sign-up-dialog-component/sign-up-dialog-component.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    CommonModule,
-    RouterOutlet,
-    AmplifyAuthenticatorModule,
-  ],
+  imports: [CommonModule, RouterOutlet, SignUpDialogComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
