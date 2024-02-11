816344f5ab9261b01c41cb96236153db932e2c88
Dohyun Kim <56704790+kdhminime@users.noreply.github.com>
2024-02-10 17:30:41 -0700

    animation

--- src/app/components/sign-up-confirm-dialog/sign-up-confirm-dialog.component.ts
+++ src/app/components/sign-up-confirm-dialog/sign-up-confirm-dialog.component.ts
@@ -1,13 +1,66 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+
+// import components
+import { LogoImageComponent } from '../logo-image/logo-image.component';
 
 @Component({
   selector: 'app-sign-up-confirm-dialog',
   standalone: true,
-  imports: [FormsModule],
+  imports: [LogoImageComponent, FormsModule],
   templateUrl: './sign-up-confirm-dialog.component.html',
-  styleUrl: './sign-up-confirm-dialog.component.scss'
+  styleUrl: './sign-up-confirm-dialog.component.scss',
+  animations: [
+    trigger('slideInOut', [
+      state(
+        'in',
+        style({
+          transform: 'translateX(0)',
+          opacity: 1,
+        })
+      ),
+      state(
+        'out',
+        style({
+          transform: 'translateX(100%)',
+          opacity: 0,
+        })
+      ),
+      transition('in => out', animate('600ms ease-out')),
+      transition('out => in', animate('600ms ease-in')),
+    ]),
+  ],
 })
 export class SignUpConfirmDialogComponent {
-  public confirmationCode: string = '';
+  public input1 = '';
+  public input2 = '';
+  public input3 = '';
+  public input4 = '';
+  public input5 = '';
+  public input6 = '';
+
+  public animationState: string = 'out';
+
+  public ngOnInit(): void {
+    setTimeout(() => {
+      this.animationState = 'in';
+    } , 100);
+  }
+
+  public onInput(event: any, nextInput: number): void {
+    const inputElement = event.target;
+    const currentInputValue = inputElement.value;
+
+
+    // Move to the next input if the current input is filled
+    if (currentInputValue.length === 1) {
+      const nextInputId = `input${nextInput + 1}`;
+      const nextInputElement = document.getElementById(nextInputId) as HTMLInputElement;
+
+      if (nextInputElement) {
+        nextInputElement.focus();
+      }
+    }
+  }
 }
